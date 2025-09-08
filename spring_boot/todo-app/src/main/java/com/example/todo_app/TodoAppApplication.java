d4100643f15ff0a4e084ec5b50f57b74b339785b
jasurbek <jasurbekibnxusniddin@gmail.com>
2025-09-09 00:38:03 +0500

    start

--- spring_boot/todo-app/src/main/java/com/example/todo_app/TodoAppApplication.java
+++ spring_boot/todo-app/src/main/java/com/example/todo_app/TodoAppApplication.java
@@ -2,11 +2,15 @@ package com.example.todo_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import system;
 
 @SpringBootApplication
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
+
+		//system.out.println("-----------Hello, World!");
+
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
